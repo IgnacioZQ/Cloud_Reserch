95f17f025e80641960bea76bee20f773173e0a21
IgnacioZQ <Ignacio.zamora@mail.udp.cl>
2021-01-16 21:39:39 -0300

    Update Data_principal.R

--- Proyecto_1_Covid_y_Dolar/Data_principal.R
+++ Proyecto_1_Covid_y_Dolar/Data_principal.R
@@ -2,23 +2,42 @@
 
 {library(RCurl)
 library(readxl)
-library(tidyverse)}
+library(tidyverse)
+library(ggrepel)}
 
-# Cargar Data de Covid ----
+# Cargar Data ----
 
-{Covid <- getURL("https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto5/TotalesNacionales_T.csv")
-Covid <- read.csv(text = Covid)}
+dolar= read_excel("Dolar.xls")
 
-# Cargar Data de Dolar ----
+Covid <- select(Covid, Fecha, Casos.totales)
+Covid$Fecha <- as.Date(as.character(Covid$Fecha))
 
-Dolar <- read_excel("Dolar.xls")
+Covid= select(Covid, Fecha) %>%
+  filter(Fecha <="2020-12-30")
+Dolar= select(Dolar, Periodo) %>%
+  filter(Periodo >"2020-03-01")
 
-# Análisis exploratorio ----
+########
 
-# Dolar
+ggplot(data = Covid, aes(x = Fecha, y = Casos.totales)) +
+  geom_point(colour = "red") +
+  ggtitle("Diagrama de dispersión") +
+  theme_bw() +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  scale_y_continuous(breaks = c(100000, 200000, 300000, 400000, 500000, 600000, 700000), labels = scales::label_number()) +
+  geom_smooth(method = 'lm',formula = 'y ~ x', se = FALSE, color = "black")
 
-summary(Dolar$Dólar)
+########
 
-plot(Dolar)
+ggplot(data = Dolar, aes(x = Periodo, y = Dólar)) +
+geom_point(colour = "red") +
+  ggtitle("Diagrama de dispersión") +
+  theme_bw() +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  scale_y_continuous(labels = scales::dollar_format(), breaks = c(550, 600, 650, 700, 750, 800, 850, 900)) +
+  geom_smooth(method = 'lm',formula='y ~ x', se = FALSE, color="black")
 
-# Covid
+######## Juntar data
+
+data_1 <- merge(Dolar, Covid)
+
