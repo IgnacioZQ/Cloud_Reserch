925f33ef671a7636bc76dd5663c3e78d59381433
IgnacioZQ <Ignacio.zamora@mail.udp.cl>
2021-01-18 22:07:25 -0300

    Update Data_principal.R

--- Proyecto_1_Covid_y_Dolar/Data_principal.R
+++ Proyecto_1_Covid_y_Dolar/Data_principal.R
@@ -5,41 +5,37 @@ library(readxl)
 library(tidyverse)
 library(ggrepel)
 library(lubridate)
-install.packages("gifski")
 library(gifski)
+library(readr)
 
 # Cargar Data ----
 
+Covid <- read.csv("https://raw.githubusercontent.com/IgnacioZQ/Cloud_Reserch/main/Proyecto_1_Covid_y_Dolar/covid.csv", fileEncoding = "UTF-8-BOM", stringsAsFactors = FALSE)
+
 Dolar = read_excel("Dolar.xls")
 
-Covid <- getURL("https://raw.githubusercontent.com/IgnacioZQ/Cloud_Reserch/main/Proyecto_1_Covid_y_Dolar/covid.csv")
-  Covid <- read.csv(text = Covid)
+# Manipular Data
 
 Covid <- select(Covid, Fecha, Casos.totales)
 Covid$Fecha <- as.Date(as.character(Covid$Fecha))
 
-Covid= select(Covid, Fecha) %>%
+{Covid = select(Covid, Fecha, Casos.totales) %>%
   filter(Fecha <="2020-12-30")
-Dolar= select(Dolar, Periodo) %>%
-  filter(Periodo >"2020-03-01")
+Dolar = select(Dolar, Periodo, Dólar) %>%
+  filter(Periodo >"2020-03-01")}
 
 Covid <- Covid %>%
-  mutate(Fecha = ymd(Fecha)) %>%
-  select(-d)
+  mutate(Fecha = ymd(Fecha))
 
 Dolar <- Dolar %>%
   mutate(Fecha = ymd(Periodo)) %>%
   select(-Periodo, Fecha, Dólar)
 
-# Unir Fechas
+# Unir Fechas en una sola Data (New_DF)
 
-New_Dt <- full_join(Covid, Dolar, by = "Fecha")
+New_DF <- full_join(Covid, Dolar, by = "Fecha")
 
-# LUBRIDATE
-
-mean(is.na(New_Dt$Dólar))
-
-########
+######## Gráfico solo Covid.
 
 ggplot(data = Covid, aes(x = Fecha, y = Casos.totales)) +
   geom_point(colour = "red") +
@@ -49,20 +45,18 @@ ggplot(data = Covid, aes(x = Fecha, y = Casos.totales)) +
   scale_y_continuous(breaks = c(100000, 200000, 300000, 400000, 500000, 600000, 700000), labels = scales::label_number()) +
   geom_smooth(method = 'lm',formula = 'y ~ x', se = FALSE, color = "black")
 
-########
+######## Gráfico solo Dolar.
 
 ggplot(data = Dolar, aes(x = Fecha, y = Dólar)) +
 geom_point(colour = "red") +
-  ggtitle("Diagrama de dispersión") +
+  ggtitle("N° de casos de Coronavirus año 2020 Chile") +
   theme_bw() +
   theme(plot.title = element_text(hjust = 0.5)) +
   scale_y_continuous(labels = scales::dollar_format(), breaks = c(550, 600, 650, 700, 750, 800, 850, 900)) +
   geom_smooth(method = 'lm',formula='y ~ x', se = FALSE, color = "black") +
-  transition_reveal(Fecha) +
   ylab("Valor en CLP")
 
-######## Juntar data
+######## Gráfico ambos juntos.
 
-data_1 <- merge(Dolar, Covid)
 
 
